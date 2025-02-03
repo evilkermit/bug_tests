4410a213709ea62a2c79bf3b0543dcbb9d88cc26
John Duggan <dugganjw@ornl.gov>
2025-02-03 12:44:21 -0500

    Prepare PyVista sample code

--- src/bug_test/views/main.py
+++ src/bug_test/views/main.py
@@ -1,15 +1,15 @@
 """Application for reproducing potential upstream bugs."""
 
-import plotly.graph_objects as go
-from plotly.data import iris, wind
+import pyvista as pv
+from pyvista import examples
+from pyvista.trame.ui import plotter_ui
 from trame.app import get_server
 from trame.decorators import TrameApp
 from trame.ui.vuetify3 import VAppLayout
-from trame.widgets import plotly
+from trame.widgets import html
 from trame.widgets import vuetify3 as vuetify
 
-IRIS_DATA = iris()
-WIND_DATA = wind()
+KNEE = examples.download_knee_full()
 
 
 @TrameApp()
@@ -18,38 +18,24 @@ class TestApp:
 
     def __init__(self) -> None:
         self.server = get_server(None, client_type="vue3")
-        self.server.state.figure = "Figure 1"
-
-        self.create_figures()
+        self.create_plotter()
 
         with VAppLayout(self.server) as layout:
             with layout.root:
-                vuetify.VSelect(
-                    v_model="figure",
-                    items=("['Figure 1', 'Figure 2']",),
-                    update_modelValue="trigger('show_figure', [figure])",
-                )
-                self.figure = plotly.Figure()
+                vuetify.VBtn("Set Colormap to Plasma", color="green", click=self.change_colormap)
+                with html.Div(classes="d-flex flex-row h-100"):
+                    self.view1 = plotter_ui(self.plotter1)
+                    self.view2 = plotter_ui(self.plotter2)
 
-            @self.server.controller.trigger("show_figure")
-            def _show_figure(name: str) -> None:
-                self.show_figure(name)
+    def create_plotter(self) -> None:
+        self.plotter1 = pv.Plotter(off_screen=True)
+        self.plotter2 = pv.Plotter(off_screen=True)
+        self.mesh = self.plotter1.add_mesh(KNEE, colormap="viridis", opacity="sigmoid", show_scalar_bar=False)
+        self.volume = self.plotter2.add_volume(KNEE, colormap="viridis", opacity="sigmoid", show_scalar_bar=False)
+        print(type(self.mesh), type(self.volume))
 
-        self.show_figure("Figure 1")
-
-    def create_figures(self) -> None:
-        self.figure_1 = go.Figure(
-            go.Heatmap(x=IRIS_DATA["sepal_width"], y=IRIS_DATA["petal_width"], z=IRIS_DATA["petal_length"])
-        )
-        self.figure_2 = go.Figure(
-            go.Heatmap(x=WIND_DATA["direction"], y=WIND_DATA["strength"], z=WIND_DATA["frequency"])
-        )
-
-    def show_figure(self, name: str) -> None:
-        match name:
-            case "Figure 1":
-                self.figure.update(self.figure_1)
-            case "Figure 2":
-                self.figure.update(self.figure_2)
-            case _:
-                raise ValueError("Figure not found.")
+    def change_colormap(self) -> None:
+        self.mesh.mapper.lookup_table.cmap = "coolwarm"
+        self.view1.update()
+        self.volume.mapper.lookup_table.cmap = "coolwarm"
+        self.view2.update()
